3dfe8bae0e7d4ca266e7c05c9f0bd77bfeafcb86
Melanie Magin <magin@cyberkombinat.de>
2022-02-01 00:25:51 +0100

    declared class RemoteControl

---
+++ src/RemoteControl/RemoteControl.cpp
@@ -0,0 +1,15 @@
+/*
+ * RemoteControl.cpp
+ *
+ *  Created on: Jan 31, 2022
+ *      Author: Melanie Magin
+ */
+#include "RemoteControl.hpp"
+
+void RemoteControl::init()
+{
+}
+
+void RemoteControl::run()
+{
+}
---
+++ src/RemoteControl/RemoteControl.hpp
@@ -0,0 +1,17 @@
+/*
+ * RemoteControl.hpp
+ *
+ *  Created on: Jan 31, 2022
+ *      Author: Melanie Magin
+ */
+#pragma once
+
+class RemoteControl
+{
+private:
+    RemoteControl() = default;
+
+public:
+    static void init();
+    static void run();
+};
