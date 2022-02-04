b369e9b3edc01f9d083f9a114cbd5366387b93ec
Melanie Magin <m.magin@cyberkombinat.de>
2022-02-05 00:12:02 +0100

    configured IO pins

--- boardConfig.h
+++ boardConfig.h
@@ -13,20 +13,20 @@
 #define ADDRESS_OF_REMOTE_CONTROLLER 1
 #define BROADCAST_ADDRESS 255
 
-#define ADDRESS_PIN_0 0
-#define ADDRESS_PIN_1 1
-#define ADDRESS_PIN_2 2
-#define ADDRESS_PIN_3 3
-#define ADDRESS_PIN_4 4
-#define ADDRESS_PIN_5 5
-#define ADDRESS_PIN_6 6
-#define ADDRESS_PIN_7 7
+#define ADDRESS_PIN_0 14
+#define ADDRESS_PIN_1 12
+#define ADDRESS_PIN_2 13
+//#define ADDRESS_PIN_3 15 // has external pulldown
+#define ADDRESS_PIN_3 10
+#define ADDRESS_PIN_4 9
+//#define ADDRESS_PIN_6 3
+//#define ADDRESS_PIN_7 1
 
-#define MOTOR_A_DIRPIN 3
-#define MOTOR_A_PWN_PIN 1
+#define MOTOR_A_DIRPIN 0
+#define MOTOR_A_PWN_PIN 5
 
-#define MOTOR_B_DIRPIN 4
-#define MOTOR_B_PWM_PIN 2
+#define MOTOR_B_DIRPIN 2
+#define MOTOR_B_PWM_PIN 4
 
 // #define SENSE_CONTROLLER_PIN 9
 // #define ON_SWITCH_PIN 10
