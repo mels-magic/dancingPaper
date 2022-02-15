1a7176176d0671b2cac53f5b88e9f09b70f4acca
Melanie Magin <m.magin@cyberkombinat.de>
2022-02-15 21:25:05 +0100

    chaned pin definitions

--- boardConfig.h
+++ boardConfig.h
@@ -6,37 +6,30 @@
  */
 #pragma once
 
+// set per devicebefore flashing:
+#define ADDRESS_NUMBERR_ON_LABEL 2
+#define IS_REMOTE_CONTROLLER true // set true / false
+
 #define MESH_PREFIX "whateverYouLike"
 #define MESH_PASSWORD "somethingSneaky"
 #define MESH_PORT 5555
 
-#define ADDRESS_PIN_0 14
-#define ADDRESS_PIN_1 12
-#define ADDRESS_PIN_2 13
-//#define ADDRESS_PIN_3 15 // has external pulldown
-#define ADDRESS_PIN_3 10
-#define ADDRESS_PIN_4 9
-//#define ADDRESS_PIN_6 3
-//#define ADDRESS_PIN_7 1
+// #define ADDRESS_PIN_0 14 // D5
+// #define ADDRESS_PIN_1 12 // D6
+// #define ADDRESS_PIN_2 13 // D7
+// //#define ADDRESS_PIN_3 15 // D8 -- has external pulldown
+// #define ADDRESS_PIN_3 10 // SD3
+// #define ADDRESS_PIN_4 9 // SD2
 
-#define MOTOR_A_DIRPIN 0
-#define MOTOR_A_PWM_PIN 5
+// #define ADDRESS_PIN_REMOTE 6 // SDCLK / CLK
+// #define ADDRESS_PIN_REMOTE_GND 7 // SDD0 / SD0 / MISO
+#define REMOTE_SWITCH_PIN 11 // SDCMD / CMD / MOSI
+// #define REMOTE_SWITCH_PIN_GND 8 // SDD1 / SD1 / INT
 
-#define MOTOR_B_DIRPIN 2
-#define MOTOR_B_PWM_PIN 4
+#define MOTOR_A_DIRPIN 0 // D3
+#define MOTOR_A_PWM_PIN 5 // D1
 
-#define REMOTE_LED_1_PIN 16
-#define REMOTE_LED_2_PIN 14
-#define REMOTE_LED_3_PIN 12
-#define REMOTE_LED_4_PIN 13
-#define REMOTE_LED_5_PIN 15
+#define MOTOR_B_DIRPIN 2 // D4, also LED near antenna
+#define MOTOR_B_PWM_PIN 4 // D2
 
-#define REMOTE_SWITCH_PIN 4
-#define REMOTE_ENCODER_BUTTON_PIN 5
-#define REMOTE_ENCODER_PIN_1 0
-#define REMOTE_ENCODER_PIN_2 2
-
-// #define SENSE_CONTROLLER_PIN 9
-// #define ON_SWITCH_PIN 10
-// #define POTI_PIN A0
-// #define LED_PIN 2
+#define REMOTE_LED_1_PIN 16  // onboard LED near USB connector
