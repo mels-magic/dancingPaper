e25464ba493798c2719bad25ffe584db69315b6e
Melanie Magin <m.magin@cyberkombinat.de>
2022-02-15 21:37:37 +0100

    updated switch pin and removed unused defines

--- boardConfig.h
+++ boardConfig.h
@@ -14,17 +14,7 @@
 #define MESH_PASSWORD "somethingSneaky"
 #define MESH_PORT 5555
 
-// #define ADDRESS_PIN_0 14 // D5
-// #define ADDRESS_PIN_1 12 // D6
-// #define ADDRESS_PIN_2 13 // D7
-// //#define ADDRESS_PIN_3 15 // D8 -- has external pulldown
-// #define ADDRESS_PIN_3 10 // SD3
-// #define ADDRESS_PIN_4 9 // SD2
-
-// #define ADDRESS_PIN_REMOTE 6 // SDCLK / CLK
-// #define ADDRESS_PIN_REMOTE_GND 7 // SDD0 / SD0 / MISO
-#define REMOTE_SWITCH_PIN 11 // SDCMD / CMD / MOSI
-// #define REMOTE_SWITCH_PIN_GND 8 // SDD1 / SD1 / INT
+#define REMOTE_SWITCH_PIN 14 // D5
 
 #define MOTOR_A_DIRPIN 0 // D3
 #define MOTOR_A_PWM_PIN 5 // D1
